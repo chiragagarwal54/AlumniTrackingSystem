9f70ab499a668eb9139cdabcfdeab1c94bb1b1e4
Himanshu Shekhar <himanshu.shekharjha.272@gmail.com>
2020-07-23 17:04:31 +0530

    [ADD] Chat room css

--- chat/static/chat/js/main.js
+++ chat/static/chat/js/main.js
@@ -7,15 +7,6 @@ $(".expand-button").click(function () {
     $("#contacts").toggleClass("expanded");
 });
 
-$('.messages').stop().animate({
-    scrollTop: $('.messages')[0].scrollHeight
-}, 800);
-
-function scroll() {
-    $('.messages').stop().animate({
-        scrollTop: $('.messages')[0].scrollHeight
-    }, 800);
-}
 
 function scroll_up() {
     $('.messages').animate({ scrollTop: 0 }, 'fast');
@@ -28,3 +19,167 @@ $('.submit').click(function () {
 $('#check_older_messages').click(function () {
     scroll_up();
 });
+
+var new_uri = window.location.protocol === 'https://' ? 'wss://' : 'ws://';
+const chatSocket = new ReconnectingWebSocket(
+    new_uri + window.location.host +
+    '/ws/chat/' + roomName + '/');
+
+chatSocket.onopen = function (e) {
+    fetchMessages(true);
+}
+
+chatSocket.onmessage = function (e) {
+    var data = JSON.parse(e.data);
+    console.log(data);
+    if (data['command'] === 'messages') {
+
+        var ChatLog = document.querySelector('#chat-log');
+        var elements = ChatLog.getElementsByTagName('li');
+        while (elements[0]) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+
+        for (let i = 0; i < data['messages'].length; i++) {
+            createMessage(data['messages'][i]);
+        }
+        if (old_msg_page_count == 0) {
+            scroll();
+        }
+
+    } else if (data['message']['command'] === 'new_message') {
+        if (old_msg_page_count != 0) {
+            old_msg_page_count = 0;
+            fetchMessages(true);
+        }
+        createMessage(data['message']['message']);
+        scroll();
+    }
+};
+
+chatSocket.onclose = function (e) {
+    console.error('Chat socket closed unexpectedly');
+};
+
+document.querySelector('#chat-message-input').onkeyup = function (e) {
+    if (e.keyCode === 13) {
+        document.querySelector('#chat-message-submit').click();
+    }
+};
+
+document.querySelector('#chat-message-submit').onclick = function (e) {
+    const messageInputDom = document.querySelector('#chat-message-input');
+    const message = messageInputDom.value;
+    if (message !== "") {
+        chatSocket.send(JSON.stringify({
+            'message': message,
+            'from': username,
+            'command': 'new_message',
+            'roomName': roomName
+        }));
+        messageInputDom.value = '';
+    }
+};
+
+function fetchMessages(recent) {
+    if (recent) {
+        chatSocket.send(JSON.stringify({ 'command': 'fetch_messages', 'roomName': roomName }));
+    }
+    chatSocket.send(JSON.stringify({ 'command': 'fetch_old_messages', 'roomName': roomName }));
+}
+
+function convertToMonth(index) {
+    switch (index) {
+        case 1:
+            return 'Jan'
+        case 2:
+            return 'Feb'
+        case 3:
+            return 'Mar'
+        case 4:
+            return 'Apr'
+        case 5:
+            return 'May'
+        case 6:
+            return 'Jun'
+        case 7:
+            return 'Jul'
+        case 8:
+            return 'Aug'
+        case 9:
+            return 'Sep'
+        case 10:
+            return 'Oct'
+        case 11:
+            return 'Nov'
+        case 12:
+            return 'Dec'
+    }
+}
+
+function formatDate(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+
+    return date.getDate() + " " + convertToMonth(date.getMonth() + 1) + "  " + strTime;
+}
+
+function createMessage(data) {
+    var author = data['author'];
+    var dateTag = document.createElement('p');
+    var imgTag = document.createElement('img');
+    var body = document.createElement('div');
+    var headerTag = document.createElement('div');
+    var nameTag = document.createElement('strong')
+    var msgTag = document.createElement('p');
+    var contain = document.createElement('div');
+    var boxTag = document.createElement('div');
+
+    var date_posted = new Date(data.timestamp);
+
+    nameTag.className = 'small text-muted';
+    nameTag.textContent = author;
+
+    msgTag.textContent = data.content;
+
+    dateTag.textContent = formatDate(date_posted);
+
+    headerTag.className = 'header';
+
+    if (!(author === first_name)) {
+        contain.className = "media w-25 mb-3";
+        contain.appendChild(imgTag);
+        imgTag.src = data["author_profile_img"];
+        imgTag.width = "50";
+        imgTag.className = "rounded-circle";
+        body.className = "media-body ml-3";
+        contain.appendChild(body);
+        body.appendChild(nameTag);
+        body.appendChild(boxTag);
+        boxTag.className = "bg-light rounded py-2 px-3 mb-2";
+        msgTag.className = "text-small mb-0 text-muted";
+        msgTag.style = "overflow-wrap: break-word; max-width: 20em";
+        boxTag.appendChild(msgTag);
+        body.appendChild(dateTag);
+        dateTag.className = "small text-muted";
+    } else {
+        contain.className = "media w-25 ml-auto mb-3";
+        contain.appendChild(body);
+        body.className = "media-body";
+        body.appendChild(boxTag);
+        boxTag.className = "bg-primary rounded py-2 px-3 mb-2";
+        boxTag.appendChild(msgTag);
+        msgTag.className = "text-small mb-0 text-white";
+        msgTag.style = "overflow-wrap: break-word; max-width: 20em";
+        body.appendChild(dateTag);
+        dateTag.className = "small text-muted";
+    }
+    document.querySelector('#chat-log').appendChild(contain);
+    var d = $('#chat-log');
+    d.scrollTop(d.prop("scrollHeight"));
+}
