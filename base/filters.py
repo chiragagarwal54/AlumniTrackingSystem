fbd910f59b3b503fb1043c7a95fdf1f8dc5b9fd1
Chirag Agarwal <chirag@gamil.com>
2020-08-01 14:34:24 +0530

    [ADD] filters, ui

--- base/filters.py
+++ base/filters.py
@@ -1,5 +1,5 @@
 from accounts.models import User, Alumni
-from college.models import College, Department
+from college.models import College, Department, Course, Specialization
 import django_filters
 
 class UserFilter(django_filters.FilterSet):
@@ -7,7 +7,10 @@ class UserFilter(django_filters.FilterSet):
     first_name = django_filters.CharFilter(lookup_expr='icontains')
     last_name = django_filters.CharFilter(lookup_expr='icontains')
     college = django_filters.ModelChoiceFilter(queryset=College.objects.all())
+    department = django_filters.ModelChoiceFilter(queryset=Department.objects.all())
+    course = django_filters.ModelChoiceFilter(queryset=Course.objects.all())
+    specialization = django_filters.ModelChoiceFilter(queryset=Specialization.objects.all())
 
     class Meta:
         model = User
-        fields = ['full_name', 'first_name', 'last_name', 'college', ]
+        fields = ['full_name', 'first_name', 'last_name', 'college', 'department', 'course', 'specialization']
